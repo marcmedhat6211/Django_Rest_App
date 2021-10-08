abd730dbde8e72742223be572dc106d4878a4b6e
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-10-08 15:09:13 +0200

    Class Based Views

--- api_demo/urls.py
+++ api_demo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_detail, article_list
+from .views import ArticleDetails, article_detail, article_list, ArticleAPIView
 
 urlpatterns = [
-    path('article/', article_list),
-    path('article/details/<int:pk>/', article_detail),
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    # path('article/details/<int:pk>/', article_detail),
+    path('article/details/<int:id>/', ArticleDetails.as_view()),
 ]
