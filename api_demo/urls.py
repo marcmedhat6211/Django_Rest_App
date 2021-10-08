b2f79a009e91b4c2775571b2201a41faad5f736f
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-10-08 16:06:12 +0200

    Generic API Views

--- api_demo/urls.py
+++ api_demo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ArticleDetails, article_detail, article_list, ArticleAPIView
+from .views import ArticleDetails, GenericAPIView, article_detail, article_list, ArticleAPIView
 
 urlpatterns = [
     # path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),
     # path('article/details/<int:pk>/', article_detail),
     path('article/details/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
+    # path('generic/article/<int:id>', GenericAPIView.as_view())
 ]
