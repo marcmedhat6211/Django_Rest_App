73cba3995c9af0a399063abefc8af2627f550326
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-10-08 17:20:29 +0200

    Generic Viewsets and Model View Sets

--- api_demo/views.py
+++ api_demo/views.py
@@ -16,32 +16,52 @@ from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 
-class ArticleViewSet(viewsets.ViewSet):
-    def list(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many = True)
-        return Response(serializer.data)
+# class ArticleViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many = True)
+#         return Response(serializer.data)
 
-    def create(self, request):
-        serializer = ArticleSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+#     def create(self, request):
+#         serializer = ArticleSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, pk = None):
-        queryset = Article.objects.all()
-        article = get_object_or_404(queryset, pk = pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk = None):
+#         queryset = Article.objects.all()
+#         article = get_object_or_404(queryset, pk = pk)
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
 
-    def update(self, request, pk = None):
-        article = Article.objects.get(pk = pk)
-        serializer = ArticleSerializer(article, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+#     def update(self, request, pk = None):
+#         article = Article.objects.get(pk = pk)
+#         serializer = ArticleSerializer(article, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+#     def destroy(self, request, pk = None):
+#         article = Article.objects.get(pk = pk)
+#         article.delete()
+#         return Response(status = status.HTTP_204_NO_CONTENT)
+
+# class ArticleViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+    ):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+# class ArticleViewSet(viewsets.ModelViewSet): # if you inherited from the model view set you don't have to inject inherit from all the above classes in your class and it will just give you all the functionalities
+
+
 
     
 
